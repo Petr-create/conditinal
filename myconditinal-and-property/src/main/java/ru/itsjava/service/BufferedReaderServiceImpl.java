080a8d33f564bac9619fe91a1cd2360671820d7d
Petr Dankin <petr2768@yandex.ru>
2021-02-24 17:12:11 +0300

    читаем menu.txt2

--- myconditinal-and-property/src/main/java/ru/itsjava/service/BufferedReaderServiceImpl.java
+++ myconditinal-and-property/src/main/java/ru/itsjava/service/BufferedReaderServiceImpl.java
@@ -16,26 +16,31 @@ public class BufferedReaderServiceImpl implements BufferedReaderService{
     @Override
     @SneakyThrows
     public void menuReader(String fileName) {
-//        BufferedReader br = newBufferedReader(Paths.get(fileName));
+//        BufferedReader br = new BufferedReader(Paths.get(fileName));
 //        String line;
 //        while((line = br.readLine()) != null){
 //            System.out.println(line);
 //        }
-        BufferedInputStream result = (BufferedInputStream)
-                Config.class.getClassLoader().getResourceAsStream(fileName);
-        byte [] b = new byte[5000];
-        int val = 0;
-        String txt = null;
-        do {
-            try {
-                val = result.read(b);
-                if (val > 0) {
-                    txt += new String(b, 0, val);
-                }
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } while (val > -1);
-        System.out.println(txt);
+//        BufferedInputStream result = (BufferedInputStream)
+//                Config.class.getClassLoader().getResourceAsStream(fileName);
+//        byte [] b = new byte[5000];
+//        int val = 0;
+//        String txt = null;
+//        do {
+//            try {
+//                val = result.read(b);
+//                if (val > 0) {
+//                    txt += new String(b, 0, val);
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//        } while (val > -1);
+//        System.out.println(txt);
+        BufferedReader br = new BufferedReader(BufferedReaderServiceImpl.class.getResourceAsStream(fileName));
+        String line;
+        while((line = br.readLine()) != null){
+            System.out.println(line);
+        }
     }
 }
