7fef1659686a0d298c7bb8426c05d9414b5c5921
Petr Dankin <petr2768@yandex.ru>
2021-03-01 21:32:19 +0300

    тестирование не получается

--- myconditinal-and-property/src/test/java/ru/itsjava/service/CoffeeServiceImplTest.java
+++ myconditinal-and-property/src/test/java/ru/itsjava/service/CoffeeServiceImplTest.java
@@ -1,19 +1,46 @@
 package ru.itsjava.service;
 
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.mockito.Mockito.when;
+
 @SpringBootTest
+@DisplayName("Класс CoffeeServiceImpl должен: ")
 public class CoffeeServiceImplTest {
 
+    private ByteArrayOutputStream output = new ByteArrayOutputStream();
+    ScannerService scannerService = Mockito.mock(ScannerService.class);
+
     @Autowired
-    MenuReaderService bufferedReaderService;
-    @Autowired
-    ScannerService scannerService;
+    CoffeeService coffeeService;
+
+    @Before
+    public void setUpStreams() {
+        System.setOut(new PrintStream(output));
+    }
 
     @Test
-    public void getCoffeeByPrice(){
-
+    @DisplayName("иметь корректный метод getCoffeeByPrice")
+    public void shouldHaveCorrectChooseCoffee(){
+        when(scannerService.readPrice()).thenReturn(60);
+        coffeeService.getCoffeeByPrice();
+        Assert.assertEquals("Эспрессо", output.toString());
     }
+
+    @After
+    public void cleanUpStreams() {
+        System.setOut(null);
+    }
+
 }
