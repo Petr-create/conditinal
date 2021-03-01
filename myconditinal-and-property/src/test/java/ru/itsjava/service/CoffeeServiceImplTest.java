d86e2f892fb89913a766b594a88cfdd91dc15a00
Petr Dankin <petr2768@yandex.ru>
2021-03-01 17:34:06 +0300

    conditional

--- myconditinal-and-property/src/test/java/ru/itsjava/service/CoffeeServiceImplTest.java
+++ myconditinal-and-property/src/test/java/ru/itsjava/service/CoffeeServiceImplTest.java
@@ -1,10 +1,8 @@
-package ru.itsjava;
+package ru.itsjava.service;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import ru.itsjava.service.MenuReaderService;
-import ru.itsjava.service.ScannerService;
 
 @SpringBootTest
 public class CoffeeServiceImplTest {
